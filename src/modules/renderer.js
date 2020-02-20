70e4907010aa85388f7519e1e50f4e83c73edc96
Anton Zhilkin <tonyzhilkin@gmail.com>
2020-02-21 02:16:32 +0300

    Add recursive files support

--- src/modules/renderer.js
+++ src/modules/renderer.js
@@ -1,18 +1,42 @@
 const _ = require('lodash');
 
-const makeline = (obj, key, sign = '') => (sign === '' ? `\n  ${key}: ${obj[key]}` : `\n  ${sign} ${key}: ${obj[key]}`);
-
-const renderLine = (obj1, obj2, key) => {
-  if (_.has(obj1, key)) {
-    if (_.has(obj2, key)) {
-      if (obj1[key] === obj2[key]) {
-        return makeline(obj1, key);
-      }
-      return `${makeline(obj2, key, '+')}${makeline(obj1, key, '-')}`;
-    }
-    return makeline(obj1, key, '-');
+const stringify = (key, value, depth, sign) => {
+  if (_.isObject(value)) {
+    const keys = Object.keys(value);
+    const objectDepth = depth + 3;
+    const objectFields = keys.map(k => `${'  '.repeat(objectDepth)}${k}: ${value[k]}`).join('\n');
+    const stringifiedObject = `${'  '.repeat(depth)}${sign} ${key}: {\n${objectFields}\n${'  '.repeat(depth + 1)}}`;
+    return stringifiedObject;
   }
-  return makeline(obj2, key, '+');
+  return `${'  '.repeat(depth)}${sign} ${key}: ${value}`;
 };
 
-export default renderLine;
+const renderNode = (node, depth) => {
+  const {
+    status, children, key, oldValue, newValue,
+  } = node;
+  if (status === 'children') {
+    return `${'  '.repeat(depth + 1)}${key}: {\n${_.flatten(children.map(child => renderNode(child, depth + 2))).join('\n')}\n${'  '.repeat(depth + 1)}}`;
+  }
+  if (status === 'unchanged') {
+    return stringify(key, newValue, depth, ' ');
+  }
+  if (status === 'added') {
+    return stringify(key, newValue, depth, '+');
+  }
+  if (status === 'deleted') {
+    return stringify(key, oldValue, depth, '-');
+  }
+  if (status === 'edited') {
+    return [stringify(key, oldValue, depth, '-'), (stringify(key, newValue, depth, '+'))];
+  }
+  return undefined;
+};
+
+const render = (diff) => {
+  const result = `{\n${_.flatten(diff.map(node => renderNode(node, 1))).join('\n')}\n}`;
+  return result;
+};
+
+
+export default render;
