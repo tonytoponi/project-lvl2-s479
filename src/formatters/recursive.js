91478ccdb0b779d5d3e0a1a835f59f078199ed34
Anton Zhilkin <tonyzhilkin@gmail.com>
2020-03-05 23:58:10 +0300

    Refactor formatters

--- src/formatters/recursive.js
+++ src/formatters/recursive.js
@@ -1,41 +1,51 @@
 import _ from 'lodash';
 
+const step = '  ';
+
 const stringify = (key, value, depth, sign) => {
   if (_.isObject(value)) {
     const keys = Object.keys(value);
     const objectDepth = depth + 3;
-    const objectFields = keys.map(k => `${'  '.repeat(objectDepth)}${k}: ${value[k]}`).join('\n');
-    const stringifiedObject = `${'  '.repeat(depth)}${sign} ${key}: {\n${objectFields}\n${'  '.repeat(depth + 1)}}`;
-    return stringifiedObject;
+    const stringifiedObjectFields = keys.map(k => `${step.repeat(objectDepth)}${k}: ${value[k]}`).join('\n');
+    const stringifiedObject = `${sign} ${key}: {\n${stringifiedObjectFields}\n${step.repeat(depth + 1)}}`;
+    return `${step.repeat(depth)}${stringifiedObject}`;
   }
-  return `${'  '.repeat(depth)}${sign} ${key}: ${value}`;
+  const stringlifiedLine = `${step.repeat(depth)}${sign} ${key}: ${value}`;
+  return stringlifiedLine;
 };
 
 const renderNode = (node, depth) => {
-  const {
-    status, children, key, oldValue, newValue,
-  } = node;
-  if (status === 'children') {
-    return `${'  '.repeat(depth + 1)}${key}: {\n${_.flatten(children.map(child => renderNode(child, depth + 2))).join('\n')}\n${'  '.repeat(depth + 1)}}`;
-  }
-  if (status === 'unchanged') {
-    return stringify(key, newValue, depth, ' ');
-  }
-  if (status === 'added') {
-    return stringify(key, newValue, depth, '+');
-  }
-  if (status === 'removed') {
-    return stringify(key, oldValue, depth, '-');
-  }
-  if (status === 'updated') {
-    return [stringify(key, oldValue, depth, '-'), (stringify(key, newValue, depth, '+'))];
-  }
-  return undefined;
+  const renderActionsByStatus = {
+    children: ({ key, children }) => {
+      const processedChildren = _.flatten(children.map(child => renderNode(child, depth + 2))).join('\n');
+      const renderedNode = `${step.repeat(depth + 1)}${key}: {\n${processedChildren}\n${step.repeat(depth + 1)}}`;
+      return renderedNode;
+    },
+    added: ({ key, newValue }) => {
+      const renderedNode = stringify(key, newValue, depth, '+');
+      return renderedNode;
+    },
+    removed: ({ key, oldValue }) => {
+      const renderedNode = stringify(key, oldValue, depth, '-');
+      return renderedNode;
+    },
+    updated: ({ key, oldValue, newValue }) => {
+      const oldLine = stringify(key, oldValue, depth, '-');
+      const newLine = stringify(key, newValue, depth, '+');
+      const renderedNode = [oldLine, newLine];
+      return renderedNode;
+    },
+    unchanged: ({ key, newValue }) => {
+      const renderedNode = stringify(key, newValue, depth, ' ');
+      return renderedNode;
+    },
+  };
+  return renderActionsByStatus[node.status](node);
 };
 
 const render = (diff) => {
-  const result = `{\n${_.flatten(diff.map(node => renderNode(node, 1))).join('\n')}\n}`;
-  return result;
+  const renderedDiff = `{\n${_.flatten(diff.map(node => renderNode(node, 1))).join('\n')}\n}`;
+  return renderedDiff;
 };
 
 
