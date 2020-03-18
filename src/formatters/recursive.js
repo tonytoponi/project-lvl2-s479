1f7e82171e1f452f525d7d29b556a80a77debb5a
Anton Zhilkin <tonyzhilkin@gmail.com>
2020-03-18 13:59:30 +0300

    Refactor recursive formatter

--- src/formatters/recursive.js
+++ src/formatters/recursive.js
@@ -2,15 +2,18 @@ import _ from 'lodash';
 
 const step = '  ';
 
-const stringify = (key, value, depth, sign) => {
+const stringlifyLine = (key, value, depth, sign = ' ') => `${step.repeat(depth)}${sign} ${key}: ${value}`;
+
+const stringifyNode = (nodekey, value, depth, sign) => {
   if (!_.isObject(value)) {
-    const stringlifiedLine = `${step.repeat(depth + 1)}${sign} ${key}: ${value}`;
-    return stringlifiedLine;
+    const stringifiedNode = stringlifyLine(nodekey, value, depth + 1, sign);
+    return stringifiedNode;
   }
   const keys = Object.keys(value);
-  const objectDepth = depth + 4;
-  const stringifiedObject = keys.map((k) => `${step.repeat(objectDepth)}${k}: ${value[k]}`).join('\n');
-  return `${step.repeat(depth + 1)}${sign} ${key}: {\n${stringifiedObject}\n${step.repeat(depth + 2)}}`;
+  const nodeData = keys.map((key) => stringlifyLine(key, value[key], depth + 3)).join('\n');
+  const stringlifiedNodeData = `{\n${nodeData}\n${step.repeat(depth + 2)}}`;
+  const stringifiedNode = stringlifyLine(nodekey, stringlifiedNodeData, depth + 1, sign);
+  return stringifiedNode;
 };
 
 const render = (diff, diffKey = '', depth = 0) => {
@@ -21,21 +24,21 @@ const render = (diff, diffKey = '', depth = 0) => {
         return renderedChildren;
       },
       added: ({ key, newValue }) => {
-        const renderedNode = stringify(key, newValue, depth, '+');
+        const renderedNode = stringifyNode(key, newValue, depth, '+');
         return renderedNode;
       },
       removed: ({ key, oldValue }) => {
-        const renderedNode = stringify(key, oldValue, depth, '-');
+        const renderedNode = stringifyNode(key, oldValue, depth, '-');
         return renderedNode;
       },
       updated: ({ key, oldValue, newValue }) => {
-        const oldLine = stringify(key, oldValue, depth, '-');
-        const newLine = stringify(key, newValue, depth, '+');
+        const oldLine = stringifyNode(key, oldValue, depth, '-');
+        const newLine = stringifyNode(key, newValue, depth, '+');
         const renderedNode = [oldLine, newLine];
         return renderedNode;
       },
       unchanged: ({ key, newValue }) => {
-        const renderedNode = stringify(key, newValue, depth, ' ');
+        const renderedNode = stringifyNode(key, newValue, depth, ' ');
         return renderedNode;
       },
     };
